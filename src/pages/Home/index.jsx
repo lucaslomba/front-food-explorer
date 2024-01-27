14df2c11b868ea4982ca68256ce4e8b73db4081b
lucaslomba <lucasazevedolomba@gmail.com>
2024-01-27 12:20:29 -0300

    Utilizando sideMenu

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,27 @@
-import { Container } from "./styles"
+import { useState } from "react";
+
+import { Container, Content } from "./styles"
 
 import { Header } from "../../components/Header"
+import { SideMenu } from "../../components/SideMenu"
+import { Footer } from "../../components/Footer";
 
 export function Home(){
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
     return(
         <Container>
-            <Header />
+            <Header onOpenMenu={() => setMenuIsOpen(true)}/>
+            <SideMenu 
+                menuIsOpen={menuIsOpen}
+                onCloseMenu={() => setMenuIsOpen(false)}
+            />
+
+            <Content>
+                Conteudo
+            </Content>
+
+            <Footer />
         </Container>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    width: 100%;
     height: 100vh;
+
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas:
+        "header"
+        "content"
+        "footer"
+`
+
+export const Content = styled.div`
+    grid-area: content;
 `
