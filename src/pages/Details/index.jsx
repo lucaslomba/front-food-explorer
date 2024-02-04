c7a5c2c0376c3c4c0edce059589c6f5da847ed29
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-03 21:19:01 -0300

    Base

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,22 @@
-import { Container } from "./styles";
+import { useState } from "react";
+
+import { Container, Content } from "./styles";
+
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
 
 export function Details(){
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
     return (
         <Container>
-            
+            <Header onOpenMenu={() => setMenuIsOpen(true)}/>
+
+            <Content>
+
+            </Content>
+
+            <Footer />
         </Container>
     )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,5 +1,25 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.div`
-    
+    width: 100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas:
+        "header"
+        "content"
+        "footer";
+`
+
+export const Content = styled.div`
+    grid-area: content;
+
+    padding: 1.5rem 7.68rem;
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+        padding: 1.5rem 3rem;
+    }
 `
