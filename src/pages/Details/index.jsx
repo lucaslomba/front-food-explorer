53111579134bda546261f477fb485acd58bc7abe
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-04 10:57:03 -0300

    Tela de details no web

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,32 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { Container, Content } from "./styles";
+import Image from "../../assets/camarao.png"
+import { PiCaretLeft } from "react-icons/pi";
+import { FiMinus, FiPlus } from "react-icons/fi";
+import { Container, Content, Dish, DashDetails, DashIngredients, DashFooter } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { SideMenu } from "../../components/SideMenu";
+import { Button } from "../../components/Button";
 
 export function Details(){
     const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+    const [amount, setAmount] = useState(1)
+
+    function handleAddItem(){
+        if(amount < 9){
+            setAmount((state) => state + 1)
+        }
+    }
+
+    function handleRemoveItem(){
+        if(amount > 1){
+            setAmount((state) => state - 1)
+        }
+    }
 
     return (
         <Container>
@@ -18,7 +37,32 @@ export function Details(){
             />
 
             <Content>
+                <Link to={-1}><PiCaretLeft /> voltar</Link>
 
+                <Dish>
+                    <img src={Image} alt="" />
+                    
+                    <DashDetails>
+                        <h2>Spaguetti Gambe</h2>
+                        <span>Massa fresca com camarões e pesto.</span>
+
+                        <DashIngredients>
+                            <span>Camarão</span>
+                            <span>Macarrão</span>
+                            <span>Molho pesto</span>
+                            <span>Manjericão</span>
+                        </DashIngredients>
+
+                        <DashFooter>
+                            <div>
+                                <FiMinus onClick={handleRemoveItem}/>
+                                <span>0{amount}</span>
+                                <FiPlus onClick={handleAddItem}/>
+                            </div>
+                            <button type="button">incluir ∙ R$ 25,00</button>
+                        </DashFooter>
+                    </DashDetails>
+                </Dish>
             </Content>
 
             <Footer />
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -22,4 +22,104 @@ export const Content = styled.div`
     @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
         padding: 1.5rem 3rem;
     }
+
+    > a {
+        font-size: 1.5rem;
+        font-weight: bold;
+
+        color: ${({ theme }) => theme.COLORS.LIGHT300};
+
+        display: flex;
+        align-items: center;
+    }
+`
+
+export const Dish = styled.div`
+    display: flex;
+    gap: 3rem;
+
+    margin-top: 2.6rem;
+`
+
+export const DashDetails = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    gap: 1.5rem;
+
+    > h2 {
+        font-size: 2.5rem;
+        color: ${({ theme }) => theme.COLORS.LIGHT300};
+    }
+
+    > span {
+        font-size: 1.5rem;
+        color: ${({ theme }) => theme.COLORS.LIGHT300};
+    }
+`
+
+export const DashIngredients = styled.div`
+    display: flex;
+    gap: 0.75rem;
+
+    > span {
+        background-color: ${({ theme }) => theme.COLORS.DARK1000};
+        border-radius: 5px;
+        padding: 4px 8px;
+
+        color: ${({ theme }) => theme.COLORS.LIGHT100};
+
+        font-size: .875rem;
+        line-height: 1.5rem;
+    }
+`
+
+export const DashFooter = styled.div`
+    display: flex;
+    align-items: center;    
+
+    gap: 2rem;
+
+    width: 100%;
+
+    @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+        flex-direction: column;
+    }
+    
+    > div {
+        display: flex;
+        align-items: center;  
+
+        gap: .875rem;
+        font-size: 1.25rem;
+        font-weight: bold;
+
+        @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+            gap: 1.16rem;
+        }
+    
+        > svg {
+            width: 1.5rem;
+            height: 1.5rem;
+    
+            cursor: pointer;
+        }
+    }
+
+    > button {
+        background-color: ${({ theme }) => theme.COLORS.TOMATO100};
+        color: ${({ theme }) => theme.COLORS.LIGHT100};
+
+        height: 3rem;
+        border: 0;
+        padding: 0 1rem;
+        border-radius: .62rem;
+
+        font-weight: 500;
+
+        &:hover {
+            background-color: ${({ theme }) => theme.COLORS.TOMATO200};
+        }
+    }
 `
