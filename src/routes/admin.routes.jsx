b0e54a73be2065a19c882df21950f6f32af1c1a4
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-03 21:11:50 -0300

    Adicionando rotas

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom"
 
 import { Home } from '../pages/Home'
 import { NotFound } from '../pages/NotFound'
+import { Details } from "../pages/Details"
 
 export function AdminRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/details/:id" element={<Details />}/>
 
             <Route path="*" element={<NotFound />}/>
         </Routes>
--- src/routes/customer.routes.jsx
+++ src/routes/customer.routes.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom"
 
 import { Home } from '../pages/Home'
 import { NotFound } from '../pages/NotFound'
+import { Details } from "../pages/Details"
 
 export function CustomerRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/details" element={<Details />}/>
 
             <Route path="*" element={<NotFound />}/>
         </Routes>
