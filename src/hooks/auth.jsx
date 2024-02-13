13736e6b99eb3a64a67abdca7f738dfad82f9bb3
lucaslomba <lucasazevedolomba@gmail.com>
2024-02-13 18:59:50 -0300

    Remvendo lixo

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { api } from '../services/api';
 
 export const AuthContext = createContext({});
